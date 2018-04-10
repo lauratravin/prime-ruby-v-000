eb176d2ce4f17a2e218f30f0c50a67029d39c24f
Laura Messa Travin <laura.travin@gmail.com>
2018-04-10 04:26:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,16 @@
 # Add  code here!
 def prime?(num)
 
+    i = 2.
+    if num == 3
+      return true
+    else
+      while i < num
+         if num % i == 0
+           return false
+         end   
+         i += 1         
+      end      
+    return true  
+
 end
