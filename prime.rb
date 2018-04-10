50000e2db6d5761720b375dcd1ab843ecc01fe2b
Laura Messa Travin <laura.travin@gmail.com>
2018-04-10 04:49:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,17 +3,34 @@ def prime?(num)
 
     i = 2
 
-    if num == 2
+    case num
+    when num < 0
+       return false
+    when num == 2
       return true
     else
-            while i <= num/2
+      while i <= num/2
 
-               if num % i == 0 || num < 0
-                 return false
-               end
-               i += 1
-            end
-           return true
-     end
+         if num % i == 0 
+           return false
+         end
+         i += 1
+      end
+     return true
+    end    
+       
+    # end
+    # if num == 2
+    #   return true
+    # else
+    #         while i <= num/2
+    # 
+    #            if num % i == 0 
+    #              return false
+    #            end
+    #            i += 1
+    #         end
+    #        return true
+    #  end
 
 end
