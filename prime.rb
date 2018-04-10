dfedce363004e85d8167d9b119502aca04870a7e
Laura Messa Travin <laura.travin@gmail.com>
2018-04-10 04:26:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -8,9 +8,9 @@ def prime?(num)
       while i < num
          if num % i == 0
            return false
-         end   
-         i += 1         
-      end      
+         end
+         i += 1
+      end
     return true  
 
 end
