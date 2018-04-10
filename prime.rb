34dcf306d5444d66e0145cde80a7be5fdd154b7c
Laura Messa Travin <laura.travin@gmail.com>
2018-04-10 04:45:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,18 @@
 # Add  code here!
 def prime?(num)
 
-    # i = 2
-    # if num == 2
-    #   return true
-    # else
-    #         while i <= num/2
-    #           
-    #            if num % i == 0
-    #              return false
-    #            end
-    #            i += 1
-    #         end
-    #        return true
-    #  end
-    prime.each(100) {|prime| p prime}
+    i = 2
+    if num == 2
+      return true
+    else
+            while i <= num/2
     
+               if num % i == 0
+                 return false
+               end
+               i += 1
+            end
+           return true
+     end
+
 end
