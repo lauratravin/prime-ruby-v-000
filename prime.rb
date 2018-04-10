1606acba185adb32965344b6054de6af66470851
Laura Messa Travin <laura.travin@gmail.com>
2018-04-10 04:28:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,12 +5,12 @@ def prime?(num)
     if num == 3
       return true
     else
-      while i < num
-         if num % i == 0
-           return false
-         end
-         i += 1
-      end
-    return true
-  end
+            while i < num
+               if num % i == 0
+                 return false
+               end
+               i += 1
+            end
+           return true
+     end
 end
